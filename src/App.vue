811a77c1e5b8c5f3b9c63af41dc90f6d66478627
Christian Toledo <chrstiantoledo@gmail.com>
2020-12-04 14:45:52 -0300

    list all 151 pokemons

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,120 @@
 <template>
   <div id="app">
-    <div v-for="(poke, index) in pokemons" :key="index">
-      <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
+    <img src="./assets/pokemoncolor.png" width="260px" />
+    <div class="search-div my-4">
+      <input
+        v-if="!notFound"
+        type="text"
+        name=""
+        id=""
+        class="input is-rounded"
+        placeholder="Buscar Pokemon"
+        v-model.trim="pokemon"
+        @keypress.enter="search"
+        @keypress.backspace="clean"
+      />
+      <button
+        v-if="!notFound"
+        class="button is-rounded is-warning ml-2"
+        @click="search"
+      >
+        Buscar
+      </button>
     </div>
+    <p>{{ pokemonNumber }}</p>
+    <div class="poke-list">
+      <Loading v-if="loading" />
+      <div v-if="notFound">
+        <img
+          src="./assets/oficerJenny.png"
+          width="350px"
+          alt=""
+          class="mt-5 mb-2"
+        />
+        <p class="is-size-3">Desculpe, não encontramos o seu Pokémon</p>
+      </div>
+      <div v-else v-for="poke in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" />
+      </div>
+    </div>
+    <button
+      v-if="!loading"
+      class="button is-rounded is-warning ml-2 my-4"
+      @click="clean"
+    >
+      Listar Todos
+    </button>
   </div>
 </template>
 
 <script>
 import http from "./services/axios";
 import Pokemon from "./components/Pokemon.vue";
+import Loading from "./components/Loading.vue";
 export default {
   name: "App",
-  components: { Pokemon },
+  components: { Pokemon, Loading },
   data() {
     return {
       pokemons: [],
+      filteredPokemons: [],
+      pokemon: "",
+      loading: false,
+      filtered: true,
+      notFound: false,
     };
   },
 
-  methods: {},
+  methods: {
+    search() {
+      this.filteredPokemons = this.pokemons;
+      if (this.pokemon) {
+        this.filteredPokemons = this.pokemons.filter(
+          (pokemon) => pokemon.name === this.pokemon.toLowerCase()
+        );
+        if (this.filteredPokemons.length === 0) {
+          console.log(this.pokemons);
+          this.filteredPokemons = this.pokemons;
+          this.notFound = true;
+        }
+        this.pokemon = "";
+      }
+    },
 
-  created() {
-    http.get("pokemon?limit=151&offset=0").then((res) => {
-      const { results } = res.data;
-      this.pokemons = results;
-      return results;
-    });
+    clean() {
+      this.notFound = false;
+      this.filteredPokemons = this.pokemons;
+      this.pokemon = "";
+    },
+  },
+
+  computed: {
+    // searchPokemon() {
+    //   if (!this.pokemon) {
+    //     return this.pokemons;
+    //   } else {
+    //     return this.pokemons.filter(
+    //       (pokemon) => pokemon.name === this.pokemon.toLowerCase()
+    //     );
+    //   }
+    // },
+  },
+
+  mounted() {
+    this.loading = true;
+    setTimeout(() => {
+      http
+        .get("pokemon?limit=151&offset=0")
+        .then((res) => {
+          const { results } = res.data;
+          this.pokemons = results;
+          this.filteredPokemons = results;
+          return results;
+        })
+        .then(() => {
+          this.loading = false;
+        });
+    }, 5000);
   },
 };
 </script>
@@ -38,5 +127,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.search-div {
+  display: flex;
+  flex-direction: row;
+  max-width: 450px;
+  align-items: center;
+}
+
+.poke-list {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,23 +1,101 @@
 <template>
-  <div>
-    <h1>{{ num }} - {{ name | uppercase }}</h1>
-    <small>{{ url }}</small>
+  <div id="pokemon">
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img
+            @click="mudarSprite"
+            :src="isFront ? pokemon.front : pokemon.back"
+            alt="Placeholder image"
+            class="pokemon-img"
+          />
+        </figure>
+      </div>
+      <div class="card-content px-0 py-2">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">
+              {{ pokemonNum | numFormated }} - {{ name | uppercase }}
+            </p>
+            <p class="subtitle is-6">{{ pokemon.type | uppercase }}</p>
+          </div>
+        </div>
+
+        <div class="content"></div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import http from "../services/axios";
 export default {
   props: {
-    num: Number,
     name: String,
     url: String,
   },
+  data() {
+    return {
+      pokemon: {},
+      isFront: true,
+      notFound: false,
+    };
+  },
+  methods: {
+    mudarSprite() {
+      this.isFront = !this.isFront;
+    },
+  },
+
+  computed: {
+    pokemonNum() {
+      return this.url.split("/")[6];
+    },
+  },
+
   filters: {
     uppercase(value) {
-      const newName = value[0].toUpperCase() + value.slice(1);
+      const newName = value[0]?.toUpperCase() + value.slice(1);
       return newName;
     },
+
+    numFormated(value) {
+      if (value.length === 2) {
+        return `0${value}`;
+      }
+      if (value.length === 1) {
+        return `00${value}`;
+      }
+      return value;
+    },
+  },
+
+  created() {
+    try {
+      http.get(this.url).then((res) => {
+        const { sprites, types } = res.data;
+
+        const data = {
+          type: types[0]?.type.name,
+          front: sprites.front_default,
+          back: sprites.back_default,
+        };
+        this.pokemon = data;
+      });
+    } catch (error) {
+      this.notFound = true;
+    }
   },
 };
 </script>
 <style>
+#pokemon {
+  margin: 5px;
+}
+.pokemon-img {
+  cursor: pointer;
+}
+
+.card {
+  width: 250px;
+}
 </style>
